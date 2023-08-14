ddd0422e38d1ff751215ddd8408a48acf8de47f9
j <137029997+Jagacool@users.noreply.github.com>
2023-08-14 20:39:10 +0530

    Create productsData.js

---
+++ src/data/productsData.js
@@ -0,0 +1,68 @@
+const productsData = [
+    {
+        id: 1,
+        img: "/images/prod1.png",
+        rating: "★★★★",
+        title: "Headphone",
+        price: 54500,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/prod2.png",
+        rating: "★★★★★",
+        title: "Smart Watch",
+        price: 31000,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "/images/prod3.png",
+        rating: "★★★★",
+        title: "Nikon Z 50",
+        price: 84007,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "/images/prod4.png",
+        rating: "★★★★★",
+        title: "iphone",
+        price: 53570,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/prod5.png",
+        rating: "★★★★",
+        title: "keyboard",
+        price: 147990,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/prod6.png",
+        rating: "★★★★★",
+        title: "Amazon Speaker",
+        price: 54500,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "/images/prod7.png",
+        rating: "★★★★",
+        title: "Airpods",
+        price: 24990,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "/images/prod8.png",
+        rating: "★★★★",
+        title: "HP Laptop",
+        price: 54500,
+        quantity: 1,
+    }
+];
+
+export default productsData;
